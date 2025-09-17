e47f3b8d21aea98513971539db3b07a4190c4738
Hanh_Dev Clean <hanh.ho26@student.passerellesnumeriques.org>
2025-09-17 21:36:08 +0700

    Update jest

---
+++ src/components/Test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var jsx_runtime_1 = require("react/jsx-runtime");
+function Test() {
+    return (0, jsx_runtime_1.jsx)("div", { children: "Test" });
+}
+exports.default = Test;
---
+++ tests/App.test.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var jsx_runtime_1 = require("react/jsx-runtime");
+var react_1 = require("@testing-library/react");
+var Test_1 = __importDefault(require("../src/components/Test"));
+describe('Homepage testing', function () {
+    it('Should display Test', function () {
+        (0, react_1.render)((0, jsx_runtime_1.jsx)(Test_1.default, {}));
+        expect(react_1.screen.getByText('Test')).toBeInTheDocument();
+    });
+});
