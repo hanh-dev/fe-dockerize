94ff2f77d7f7ae2eea7d28f60439c1290d7c85d7
Hanh_Dev Clean <hanh.ho26@student.passerellesnumeriques.org>
2025-09-17 21:38:38 +0700

    Update format

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,6 +1,6 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
-var jsx_runtime_1 = require('react/jsx-runtime');
+const jsx_runtime_1 = require('react/jsx-runtime');
 function Test() {
   return (0, jsx_runtime_1.jsx)('div', { children: 'Test' });
 }
--- tests/App.test.js
+++ tests/App.test.js
@@ -1,15 +1,15 @@
 'use strict';
-var __importDefault =
+const __importDefault =
   (this && this.__importDefault) ||
   function (mod) {
     return mod && mod.__esModule ? mod : { default: mod };
   };
 Object.defineProperty(exports, '__esModule', { value: true });
-var jsx_runtime_1 = require('react/jsx-runtime');
-var react_1 = require('@testing-library/react');
-var Test_1 = __importDefault(require('../src/components/Test'));
-describe('Homepage testing', function () {
-  it('Should display Test', function () {
+const jsx_runtime_1 = require('react/jsx-runtime');
+const react_1 = require('@testing-library/react');
+const Test_1 = __importDefault(require('../src/components/Test'));
+describe('Homepage testing', () => {
+  it('Should display Test', () => {
     (0, react_1.render)((0, jsx_runtime_1.jsx)(Test_1.default, {}));
     expect(react_1.screen.getByText('Test')).toBeInTheDocument();
   });
