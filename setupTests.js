32cd22f7568123af7a8da0695b62ee0fea849479
Hanh_Dev Clean <hanh.ho26@student.passerellesnumeriques.org>
2025-09-17 21:37:16 +0700

    Update format code

--- setupTests.js
+++ setupTests.js
@@ -1,3 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("@testing-library/jest-dom");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+require('@testing-library/jest-dom');
